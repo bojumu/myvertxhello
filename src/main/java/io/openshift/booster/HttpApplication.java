2f05b4301a40c9d923cc29347daf631162151eba
bojumu <bojumu@yahoo.com>
2017-10-04 21:07:34 +0000

    I used "My Man"

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Bawoni, %s!";
+  protected static final String template = "My man, %s!";
 
   @Override
   public void start(Future<Void> future) {
